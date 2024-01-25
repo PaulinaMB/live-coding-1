678cdc7894bf20157729b14e809785711fba6dea
PaulinaMB <paulinamendozabernal@gmail.com>
2024-01-25 17:58:44 -0600

    Prueba técnica 1

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// Escribe tu código aquí.
+/*Retroalimentación:
+La desarrolladora se mostro segura de si misma, inicio por aclarar detalles sobre el problema a resolver y seguido de esto se dedicó a generar el código basándose en la lógica que había construido con su proceso metódico. 
+*/
+
+const miArrayPrueba = ["amarillo", "rojo", "verde", "morado","azul", "blanco", "naranja"];
+
+
+const filtroPalabras = (palabra, conjunto) => {
+    return conjunto.filter(word => word.length > palabra.length)
+
+};
+
+const palabra = "rosa";
+
+const resultado = filtroPalabras(palabra, filtroPalabras);
+console.log(resultado);
+
+
+//Ejercicio 2
+const ulElement = document.getElementById('miLista'); 
+
